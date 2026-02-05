2be68a4705236bc86bdf7f6585fc35787b960793
Shaheer Imran Siddiqui <shaheerimran.siddiqui2@mail.dcu.ie>
2026-02-05 19:38:06 +0000

    Add Firebase Integration with Local Environment Config #done

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -4,13 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
+import { environment } from '../environments/environment';
+import { FirebaseOptions } from '@angular/fire/app/public_api';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.FirebaseOptions as FirebaseOptions),
+    AngularFireAuthModule,
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
