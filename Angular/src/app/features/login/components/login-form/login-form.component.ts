75e6100ae6cfc0531d482cb4f2e8cad69944e504
Shaheer Imran Siddiqui <shaheerimran.siddiqui2@mail.dcu.ie>
2026-03-01 14:21:41 +0000

    Feature: add basic login page

---
+++ Angular/src/app/features/login/components/login-form/login-form.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.scss']
+})
+export class LoginFormComponent implements OnInit {
+  private fb: FormBuilder;
+  loginForm!: FormGroup;
+  constructor(fb: FormBuilder) {
+    this.fb = fb;
+  }
+
+  async ngOnInit() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+
+  }
+
+  loginAs(role: string): void {
+    void role;
+  }
+
+  
+}
---
+++ Angular/src/app/features/login/login.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+
+@NgModule({
+  declarations: [
+    LoginFormComponent
+  ],
+  imports: [
+    CommonModule,
+    LoginRoutingModule,
+        SharedModule,
+
+  ]
+})
+export class LoginModule { }
